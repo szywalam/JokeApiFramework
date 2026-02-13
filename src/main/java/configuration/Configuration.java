9fe358c2963cd1f3871a161129a33e455f5f7063
szywalam <mateusz.szywala@gmail.com>
2026-02-13 23:30:11 +0100

    AI=NO Updated .gitignore, setup of POM and Configuration class defining connection properties

---
+++ src/main/java/configuration/Configuration.java
@@ -0,0 +1,9 @@
+package configuration;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class Configuration {
+    public static final String BASE_URL = "https://v2.jokeapi.dev/joke/";
+}

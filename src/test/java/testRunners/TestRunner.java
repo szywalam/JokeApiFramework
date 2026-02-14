ab636414377573f6301af72f5d3c16d7b9c4f388
szywalam <mateusz.szywala@gmail.com>
2026-02-14 20:46:24 +0100

    AI=NO Created TestRunner for TestNG with Cucumber and Allure Reporing, created simple Allure properties file

---
+++ src/test/java/testRunners/TestRunner.java
@@ -0,0 +1,15 @@
+package testRunners;
+
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+
+@CucumberOptions(
+        features = "src/test/resources/features",
+        glue = "stepDefinitions",
+        plugin = {
+                "pretty",
+                "io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm"
+        }
+)
+public class TestRunner extends AbstractTestNGCucumberTests{
+}
